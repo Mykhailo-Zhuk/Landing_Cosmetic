c32876fc05cb2a8645c9e238493c15420c73767f
Misha-Zhuk <mishazhuk1@gmail.com>
2021-10-10 23:50:39 +0300

    Start @media + JS

--- #source/script.js
+++ #source/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(this.document).ready(function() {
     $('.top-line__headerBurger').click(function(event) {
         $('.top-line__headerBurger,.top-line__item').toggleClass('active');
         $('body').toggleClass('lock');
