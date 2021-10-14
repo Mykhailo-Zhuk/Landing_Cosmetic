47a78b3064586d7e5f0565391bf1d20b702752ca
Misha-Zhuk <mishazhuk1@gmail.com>
2021-10-14 22:56:53 +0300

    Use @media

--- #source/script.js
+++ #source/script.js
@@ -1,6 +1,6 @@
 $(this.document).ready(function() {
     $('.top-line__headerBurger').click(function(event) {
-        $('.top-line__headerBurger,.top-line__item').toggleClass('active');
+        $('.top-line__headerBurger,.top-line__item,.top-line__body_list').toggleClass('active');
         $('body').toggleClass('lock');
     });
 }); 
